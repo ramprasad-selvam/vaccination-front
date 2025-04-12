b8424f2cad451f0d8e54c1c36e8241480bbd6dd7
Ramprasad <ramprasad1@justdial.com>
2025-04-12 18:10:19 +0530

    login with api

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -4,11 +4,10 @@ function Logout() {
   const navigate = useNavigate();
   useEffect(() => {
     localStorage.clear();
-    // document.cookie = "authToken=exampleToken123; path=/; max-age=86400";
-    // document.cookie = "authToken=; path=/; max-age=0";
+    document.cookie = "authToken=; path=/; max-age=0";
     setTimeout(() => {
       navigate("/login");
-    }, 10);
+    }, 1);
   }, []);
   return <></>;
 }
